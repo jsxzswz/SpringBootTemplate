d1922e3d92f30c76fa4bfde2db326c0e97d2d71b
ExtraordinarySWZ <js_swz2008@163.com>
2019-04-22 17:04:23 +0800

    excel异常类修改

--- src/main/java/com/swz/exception/ExcelException.java
+++ src/main/java/com/swz/exception/ExcelException.java
@@ -6,23 +6,19 @@ package com.swz.exception;
  * @author: swz
  * @date: 2018/7/24 9:40
  */
-public class ExcelException extends Exception{
+public class ExcelException extends Exception {
     public ExcelException() {
-        // TODO Auto-generated constructor stub
     }
 
     public ExcelException(String message) {
         super(message);
-        // TODO Auto-generated constructor stub
     }
 
     public ExcelException(Throwable cause) {
         super(cause);
-        // TODO Auto-generated constructor stub
     }
 
     public ExcelException(String message, Throwable cause) {
         super(message, cause);
-        // TODO Auto-generated constructor stub
     }
 }
